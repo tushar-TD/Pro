2d3061205f4e132d43e2197982d356e8ac984baf
Nikhil1872 <ntalmale07@gmail.com>
2023-11-04 16:26:47 +0530

    basic react structure

---
+++ myapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App(props) {
+  return (
+    <div className="App">
+      <h1>Hello selmon Bhai!</h1>
+
+    </div>
+  );
+}
+
+export default App;
