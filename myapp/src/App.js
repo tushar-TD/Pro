249bae487ef4c0d539ba7c26fc806319f9b2cea1
Nikhil1872 <ntalmale07@gmail.com>
2023-11-05 08:58:10 +0530

    bootstrap and basics

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Header from './Components/Header';
 function App(props) {
   return (
     <div className="App">
-      <h1>Hello selmon Bhai!</h1>
+      <Header></Header>
 
     </div>
   );
