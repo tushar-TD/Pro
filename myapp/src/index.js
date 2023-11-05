ad43def6d0cbed66056e1c60eed617f45c450180
Nikhil1872 <ntalmale07@gmail.com>
2023-11-05 15:59:50 +0530

    .

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -4,10 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import Menu from './pages/Menu';
+import About from './pages/Menu';
+import Contact from './pages/Menu';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App />}></Route>
+    <Route path='/' element={<App />}>
+      <Route path='menu' element={<Menu />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+    </Route>
+
+
+
   )
 )
 
