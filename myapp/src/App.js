a875651d416638aaaf8d494e97f88655505b9fd2
Tushar-TD <dhanorkartushar2@gmail.com>
2023-11-07 18:20:10 +0530

    mongodb connected and toast to show messages on submit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -4,17 +4,20 @@ import React, { useState } from 'react';
 import StateDropdownForm from './Components/StateDropdownForm';
 import Header from './Components/Header';
 import { Outlet } from 'react-router-dom';
-
-
+import { toast, Toaster } from 'react-hot-toast'
 
 function App(props) {
   return (
-    <div >
-      <Header />
-      <main className='pt-16 bg-slate-100 min-w-[calc(100vh)]'><Outlet /></main>
-    </div >
+    <>
+      <Toaster />
+      <div>
+        <Header />
+        <main className='pt-16 bg-slate-100 min-w-[calc(100vh)]'>
+          <Outlet /> {/* Renders child routes */}
+        </main>
+      </div>
+    </>
   );
-
 }
 
 export default App;
--- myapp/src/pages/Signup.js
+++ myapp/src/pages/Signup.js
@@ -4,6 +4,7 @@ import { BiHide, BiShow } from "react-icons/bi";
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Login from './Login';
+import { toast } from "react-hot-toast"
 import { useNavigate } from 'react-router-dom';
 import { imagetoBase64 } from '../Utility/ImagetoBase64';
 const Signup = () => {
@@ -14,7 +15,7 @@ const Signup = () => {
   const handleShowPassword = () => {
     setShowPassword(preve => !preve);
   };
-  const handleShowconfirmpassword = () => {
+  const handleShowConfirmPassword = () => {
     setShowconfirmpassword(preve => !preve);
   }
   const [data, setData] = useState({
@@ -27,7 +28,7 @@ const Signup = () => {
   });
   console.log(data);
 
-  const handleOnchange = (e) => {
+  const handleOnChange = (e) => {
     const { name, value } = e.target;
     setData((prev) => ({
       ...prev,
@@ -35,7 +36,7 @@ const Signup = () => {
     }));
   };
 
-  const handleUploadprofilpic = async (e) => {
+  const handleUploadProfilePic = async (e) => {
     console.log(e.target.files[0]);
     const data = await imagetoBase64(e.target.files[0])
     console.log(data);
@@ -48,23 +49,39 @@ const Signup = () => {
       }
     })
   }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const { firstname, email, password, confirmpassword } = data
+  console.log(process.env.REACT_APP_SERVER_DOMAIN);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { firstname, email, password, confirmpassword } = data;
     if (firstname && email && password && confirmpassword) {
-      if (password == confirmpassword) {
-        alert("Done!");
-        navigate("/login");
+      if (password === confirmpassword) {
+        try {
+          const fetchData = await fetch(`${process.env.REACT_APP_SERVER_DOMAIN}/signup`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json" // Fixed typo: "content-type" to "Content-Type"
+            },
+            body: JSON.stringify(data)
+          });
+
+          const dataRes = await fetchData.json();
+          console.log(dataRes);
+          //alert(dataRes.message);
+          toast(dataRes.message);
+          // Redirect to the login page after successful signup
+          // Example: navigate("/login");
+        } catch (error) {
+          console.error("Error during signup:", error);
+          alert("Error during signup. Please try again later.");
+        }
+      } else {
+        alert("Passwords do not match. Please check again.");
       }
-      else {
-        alert("Please check data again");
-      }
+    } else {
+      alert("Please fill in all fields.");
     }
-    else {
-      alert("Please enter data");
-    }
-  }
+  };
+
 
   return (
     <div className='p-3 md:p-4 '>
@@ -74,7 +91,7 @@ const Signup = () => {
           <img src={data.profilpic ? data.profilpic : pic} className='w-full h-full' />
           <label htmlFor='profilpic'>
             <div className=' cursor-pointer absolute bottom-1 h-1/5 bg-slate-100 bg-opacity-50 w-full text-center'><p className='text-sm '>Upload</p></div>
-            <input type='file' id='profilpic' accept='images' className='hidden cursor-pointer' onChange={handleUploadprofilpic} />
+            <input type='file' id='profilpic' accept='images' className='hidden cursor-pointer' onChange={handleUploadProfilePic} />
           </label>
         </div>
 
@@ -82,24 +99,24 @@ const Signup = () => {
 
         <form className='w-full py-3 flex flex-col' onSubmit={handleSubmit} >
           <lable htmlFor='firstname' className=''>First Name</lable>
-          <input type='text' id='firstname' name='firstname' className='mt-1 w-full bg-slate-300 p-1 px-2 py-1 rounded focus-within:outline-blue-400 ' value={data.Firstname} onChange={handleOnchange} ></input>
+          <input type='text' id='firstname' name='firstname' className='mt-1 w-full bg-slate-300 p-1 px-2 py-1 rounded focus-within:outline-blue-400 ' value={data.firstname} onChange={handleOnChange} ></input>
 
           <lable htmlFor='lastname' className=''>Last Name</lable>
-          <input type='text' id='lastname' name='lastName' className='mt-1 w-full bg-slate-300 p-1 px-2 py-1 rounded  focus-within:outline-blue-400' value={data.Lastname} onChange={handleOnchange} ></input>
+          <input type='text' id='lastname' name='lastname' className='mt-1 w-full bg-slate-300 p-1 px-2 py-1 rounded  focus-within:outline-blue-400' value={data.lastname} onChange={handleOnChange} ></input>
 
           <lable htmlFor='email' >Email</lable>
-          <input type='email' id='email' name='email' className='mt-1 w-full bg-slate-300 p-1 px-2 py-1 rounded  focus-within:outline-blue-400' value={data.email} onChange={handleOnchange}></input>
+          <input type='email' id='email' name='email' className='mt-1 w-full bg-slate-300 p-1 px-2 py-1 rounded  focus-within:outline-blue-400' value={data.email} onChange={handleOnChange}></input>
 
           <lable htmlFor="password" >Password</lable>
           <div className='flex px-2 py-1 bg-slate-300 rounded mt-1  focus-within:outline-blue-400'>
-            <input type={showPassword ? 'text' : 'password'} id='password' name='password' className=' w-full bg-slate-300 p-1 border-none   focus-within:outline-blue-400' value={data.password} onChange={handleOnchange}></input>
+            <input type={showPassword ? 'text' : 'password'} id='password' name='password' className=' w-full bg-slate-300 p-1 border-none   focus-within:outline-blue-400' value={data.password} onChange={handleOnChange}></input>
             <span className='flex text-xl cursor-pointer mt-2' onClick={handleShowPassword}>{showPassword ? < BiShow /> : <BiHide />}</span>
           </div >
 
           <lable htmlFor="confirmpassword" >Confirm Password</lable>
           <div className='flex px-2 py-1 bg-slate-300 rounded mt-1  focus-within:outline-blue-400'>
-            <input type={showconfirmpassword ? 'text' : 'password'} id='confirmpassword' name='confirmpassword' className=' w-full bg-slate-300 p-1 border-none   focus-within:outline-blue-400' value={data.confirmpassword} onChange={handleOnchange} ></input>
-            <span className='flex text-xl cursor-pointer mt-2' onClick={handleShowconfirmpassword}>{showconfirmpassword ? < BiShow /> : <BiHide />}</span>
+            <input type={showconfirmpassword ? 'text' : 'password'} id='confirmpassword' name='confirmpassword' className=' w-full bg-slate-300 p-1 border-none   focus-within:outline-blue-400' value={data.confirmpassword} onChange={handleOnChange} ></input>
+            <span className='flex text-xl cursor-pointer mt-2' onClick={handleShowConfirmPassword}>{showconfirmpassword ? < BiShow /> : <BiHide />}</span>
           </div >
 
           <button type='submit' className='w-full max-w-[150px] m-auto bg-blue-500 cursor-pointer hover:bg-red-300 text-white text-xl font-medium text-center py-1 rounded-full mt-4'>Sign Up</button>
